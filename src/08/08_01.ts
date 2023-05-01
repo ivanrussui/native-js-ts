b598b644bbdd1d0004ec674d2e5f1d991a003f9e
Ivan <fenomen_ny@mail.ru>
2023-05-01 23:39:37 +0300

    08 - Ассоциативный массив JavaScript wip

---
+++ src/08/08_01.ts
@@ -0,0 +1,13 @@
+
+
+export const users = {
+  '101': {id: 101, name: 'Ivan'},
+  '3244': {id: 3244, name: 'Anna'},
+  '1': {id: 3244, name: 'Melissa'}
+}
+
+export const usersArray = [
+  {id: 101, name: 'Ivan'},
+  {id: 3244, name: 'Anna'},
+  {id: 3244, name: 'Melissa'}
+]
